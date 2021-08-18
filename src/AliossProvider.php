16fcbf97f7658c183b3932e9aa56b39a95388323
lanqiukun <1543323033@qq.com>
2021-08-18 14:31:02 +0800

    change composer

--- src/AliossProvider.php
+++ src/AliossProvider.php
@@ -6,27 +6,16 @@ use Illuminate\Support\ServiceProvider;
 
 class AliossProvider extends ServiceProvider
 {
-    protected $defer = true;
     
     public function register()
     {
-        $this->app->singleton(Direct::class, function(){
-            return new Direct;
-        });
-
-        $this->app->alias(Direct::class, 'direct');
+        
     }
 
 
     public function boot()
     {
 
-
-
     }
 
-    public function provides()
-    {
-        return [Direct::class, 'direct'];
-    }
 }
